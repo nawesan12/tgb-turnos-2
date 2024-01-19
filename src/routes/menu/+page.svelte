dc27ec0ca26f21698471f49185d7fa0eb96bc41b
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2024-01-19 16:07:21 -0300

    a

--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -1,6 +1,6 @@
 <section class="h-screen flex flex-col font-semibold items-center justify-center gap-10">
-	<a href="/perfil">Mis Turnos</a>
-	<a href="/turno">Agendar Turno</a>
+	<a class="flicker-in-2" href="/perfil">Mis Turnos</a>
+	<a class="flicker-in-2" href="/turno">Agendar Turno</a>
 </section>
 
 <style>
