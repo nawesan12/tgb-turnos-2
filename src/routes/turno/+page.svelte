4d2898d2a560e5d8f72bfd21d1ca79b6c2f9695f
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2024-11-26 15:41:24 -0300

    tuki

--- src/routes/turno/+page.svelte
+++ src/routes/turno/+page.svelte
@@ -9,6 +9,17 @@
 	<h2 class="px-4 text-2xl font-semibold">Hola, {$bookingContext.clientName}</h2>
 	<h1 class="text-center text-2xl font-bold mt-8">Agenda tu turno</h1>
 
+	<section class="bg-white p-4 font-semibold text-xl text-black m-4 mb-0 rounded-lg">
+		<p class="text-center my-4">Buscas turno para las fiestas? 🎄</p>
+
+		<a
+			href="/turno/fiestas"
+			class="bg-orange-600 rounded-lg p-2 text-white mt-8 w-full text-center"
+		>
+			Agendalo Aca
+		</a>
+	</section>
+
 	<ListOfDates />
 </section>
 
---
+++ src/routes/turno/fiestas/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+	import DateItem from '$lib/components/Bookings/DateItem.svelte';
+	import GoBack from '$lib/components/GoBack.svelte';
+
+	const navidad = [
+		{ year: 2024, month: 12, day: 23 },
+		{ year: 2024, month: 12, day: 24 }
+	];
+
+	const nuevo = [
+		{ year: 2024, month: 12, day: 30 },
+		{ year: 2024, month: 12, day: 31 }
+	];
+</script>
+
+<GoBack />
+<section>
+	<h2 class="text-center my-8 font-semibold text-2xl">Agenda tu turno para las fiestas!</h2>
+
+	<h3 class="m-4 font-semibold text-xl">Navidad 🎄</h3>
+	<div class="flex flex-wrap gap-8 justify-center">
+		{#each navidad as date}
+			<DateItem {date} />
+		{/each}
+	</div>
+
+	<h3 class="m-4 font-semibold text-xl">Año Nuevo 🙌❤</h3>
+	<div class="flex flex-wrap gap-8 justify-center">
+		{#each navidad as date}
+			<DateItem {date} />
+		{/each}
+	</div>
+</section>
