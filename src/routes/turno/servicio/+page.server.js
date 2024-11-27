b7140d0b79a4ae2093fbaf5305f9acb4c26491f5
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2024-11-27 14:05:01 -0300

    a

--- src/routes/turno/servicio/+page.server.js
+++ src/routes/turno/servicio/+page.server.js
@@ -5,6 +5,10 @@ const prisma = new PrismaClient();
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
 	const services = await prisma.service.findMany();
+	const holidayServices = await prisma.holidayService.findMany();
 
-	return { services: services.sort((a, b) => a.id - b.id) };
+	return {
+		services: services.sort((a, b) => a.id - b.id),
+		holidayServices: holidayServices.sort((a, b) => a.id - b.id)
+	};
 }
